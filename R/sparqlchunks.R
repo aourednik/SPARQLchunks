83085de865e233770b7561cfdecfec38d8754698
aourednik <andre.ourednik@epfl.ch>
2022-05-30 22:17:28 +0200

    Initial commit

---
+++ R/sparqlchunks.R
@@ -0,0 +1,95 @@
+#' Add `sparql` as knit-engine to knitr package
+#'
+#' The \code{sparql} engine can be activated via
+#'
+#' ```
+#' knitr::knit_engines$set(sparql = SPARQLchunks::eng_sparql)
+#' ```
+#'
+#' This will be set within an R Markdown document's setup chunk. Do not use eng_sparql function elsewhere.
+#'
+#' @description This package runs on top of knitr to facilitate communication with `sparql`. Run `sparql` scripts from R, R Markdown and Quarto document.
+#' @usage eng_sparql(options)
+#' @param options Chunk options, as provided by \code{knitr} during chunk execution. Chunk option for this is \code{sparql}
+#' @return Data in dataframe or list form (depending on options)
+#' @author [André Ourednik](https://ourednik.info)
+#' @examples library(SPARQLchunks)
+#' knitr::knit_engines$set(sparql = SPARQLchunks::eng_sparql)
+#' @references André Ourednik (2021). Execute SPARQL chunks in Rmarkdown Available at:  https://ourednik.info/maps/2021/12/14/execute-sparql-chunks-in-r-markdown/
+#' @family important functions
+#' @keywords documentation
+#' @export
+eng_sparql <- function(options) {
+  code <- paste(options$code, collapse = '\n')
+  ep<- options$endpoint
+  qm <- paste(ep, "?", "query", "=", gsub("\\+", "%2B", utils::URLencode(code, reserved = TRUE)), "", sep = "")
+  proxy_url <- curl::ie_get_proxy_for_url(ep)
+  proxy_config <- httr::use_proxy(url=proxy_url)
+  varname <- options$output.var
+  if(is.null(options$output.type)) {
+    output_type <- "dataframe"
+  } else {
+    output_type <- options$output.type
+  }
+  if (output_type=="list") {
+    out <- httr::GET(qm,proxy_config, httr::timeout(60)) %>% xml2::read_xml() %>% xml2::as_list()
+    nresults <- length(out$sparql$results)
+  } else {
+    queryres_csv <- httr::GET(qm,proxy_config, httr::timeout(60), httr::add_headers(c(Accept = "text/csv")))
+    out <- rawToChar(queryres_csv$content)
+    out <- textConnection(out)
+    out <- utils::read.csv(out)
+    nresults <- nrow(out)
+  }
+  chunkout <- ifelse(!is.null(varname),qm,out)
+  text <- paste("The SPARQL query returned",nresults,"results")
+  if (!is.null(varname)) assign(varname, out, envir = knitr::knit_global())
+  knitr::engine_output(options, options$code, chunkout, extra=text)
+}
+
+#' Fetch data from a SPARQL endpoint and store the output in a dataframe
+#' @param endpoint The SPARQL endpoint (a URL)
+#' @param query The SPARQL query (character)
+#' @examples library(SPARQLchunks)
+#' endpoint <- "https://lindas.admin.ch/query"
+#' query <- "PREFIX schema: <http://schema.org/>
+#'   SELECT * WHERE {
+#'   ?sub a schema:DataCatalog .
+#'   ?subtype a schema:DataType .
+#' }"
+#' result_df <- sparql2df(endpoint,query)
+#' @export
+sparql2df <- function(endpoint,query) {
+	proxy_url <- curl::ie_get_proxy_for_url(endpoint)
+	proxy_config <- httr::use_proxy(url=proxy_url)
+	qm <- paste(endpoint, "?", "query", "=", gsub("\\+", "%2B", utils::URLencode(query, reserved = TRUE)), "", sep = "")
+	queryres_csv <- httr::GET(qm,proxy_config, httr::timeout(60), httr::add_headers(c(Accept = "text/csv")))
+	out <- rawToChar(queryres_csv$content)
+	out <- textConnection(out)
+	out <- utils::read.csv(out)
+	return(out)
+}
+
+#' Fetch data from a SPARQL endpoint and store the output in a list
+#' @param endpoint The SPARQL endpoint (a URL)
+#' @param query The SPARQL query (character)
+#' @examples library(SPARQLchunks)
+#' endpoint <- "https://lindas.admin.ch/query"
+#' query <- "PREFIX schema: <http://schema.org/>
+#'   SELECT * WHERE {
+#'   ?sub a schema:DataCatalog .
+#'   ?subtype a schema:DataType .
+#' }"
+#' result_list <- sparql2list(endpoint,query)
+#' @export
+sparql2list <- function(endpoint,query) {
+	proxy_url <- curl::ie_get_proxy_for_url(endpoint)
+	proxy_config <- httr::use_proxy(url=proxy_url)
+	qm <- paste(endpoint, "?", "query", "=", gsub("\\+", "%2B", utils::URLencode(query, reserved = TRUE)), "", sep = "")
+	out <- httr::GET(qm,proxy_config, httr::timeout(60)) %>% xml2::read_xml() %>% xml2::as_list()
+	return(out)
+}
+
+.onAttach <- function(libname, pkgname) {
+	packageStartupMessage("Run `knitr::knit_engines$set(sparql = SPARQLchunks::eng_sparql)` in your Rmd setup chunk to be able to execute SPARQL chunks.")
+}
---
+++ README.md
@@ -0,0 +1,94 @@
+
+# SPARQLchunks
+
+<!-- badges: start -->
+[![Lifecycle: stable](https://img.shields.io/badge/lifecycle-stable-brightgreen.svg)](https://lifecycle.r-lib.org/articles/stages.html#stable)
+<!-- badges: end -->
+
+This package allows you to query SPARQL endpoints in two different ways: 
+
+1. It allows you to run SPARQL chunks in Rmarkdown files. 
+2. It provides inline functions to send a SPARQL queries to a user-defined endpoint and retrieve data in _dataframe_ form (`sparql2df`) or _list_ form (`sparql2list`). 
+
+Endpoints can be reached from behind corporate firewalls thanks to automatic proxy detection. See [Execute SPARQL chunks in R Markdown](https://ourednik.info/maps/2021/12/14/execute-sparql-chunks-in-r-markdown/).
+
+## Installation
+
+```{r, eval = FALSE}
+install_github("aourednik/SPARQLchunks")
+```
+
+## Use
+
+To use the full potential of the package you need to load the library and _tell knitr that a SPARQL engine exists_: 
+
+```{r setup, eval=FALSE}
+library(SPARQLchunks)
+knitr::knit_engines$set(sparql = SPARQLchunks::eng_sparql)
+```
+
+Once you have done so, you can run SPARQL chunks:
+
+### Chunks
+
+#### Retrieve a dataframe
+
+_output.var_: the name of the data.frame you want to store the results in
+
+_endpoint_: the URL of the SPARQL endpoint
+
+
+````markdown
+```{sparql output.var="queryres_csv", endpoint="https://lindas.admin.ch/query"}
+PREFIX schema: <http://schema.org/>
+SELECT * WHERE {
+  ?sub a schema:DataCatalog .
+  ?subtype a schema:DataType .
+}
+```
+````
+
+####  Retrieve a list
+
+_output.var_: the name of the data.frame you want to store the results in
+
+_endpoint_: the URL of the SPARQL endpoint
+
+_output.type_ : if set to "list", retrieves a list (tree structure) instead of a data-frame 
+
+````markdown
+```{sparql output.var="queryres_csv", endpoint="https://lindas.admin.ch/query"}
+PREFIX schema: <http://schema.org/>
+SELECT * WHERE {
+  ?sub a schema:DataCatalog .
+  ?subtype a schema:DataType .
+}
+```
+````
+
+### Inline code
+
+In all cases, you need to define an endpoint and prepare a SPQRQL query. Queries can be multi-line:
+
+```r
+endpoint <- "https://lindas.admin.ch/query"
+query <- "PREFIX schema: <http://schema.org/>
+  SELECT * WHERE {
+  ?sub a schema:DataCatalog .
+  ?subtype a schema:DataType .
+}"
+```
+
+
+#### Retrieve a list
+
+```r
+result_df <- sparql2df(endpoint,query)
+```
+
+
+#### Retrieve a data.frame
+
+```r
+result_list <- sparql2list(endpoint,query)
+```
